afcad556ffac687bd67c6f739c24bfa34cd01bea
Brandon Bilfield <brandx105@gmail.com>
2020-10-09 15:33:33 -0700

    Initial Commit of ESLint Action

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+}
---
+++ src/app.js
@@ -0,0 +1,9 @@
+const hello;
+const world;
+
+hello = w0rld;
+twerld = jello;
+
+function blahblah(){
+    alert(funky);
+} 
